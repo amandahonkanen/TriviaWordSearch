9c386e91582816afc43195fd8f5d351491331468
Amanda Honkanen <amanda.honkanen@me.com>
2017-02-02 17:53:04 +0100

    working game

--- js/application.js
+++ js/application.js
@@ -1,15 +1,81 @@
 var trivia = new TriviaWordSearch();
 
 $(document).ready(function(){
-  $('button').on('click', wrapper);
+
+
+  $('#startButton').on('click', wrapper);
   function wrapper(){
+    $('#next').toggleClass("active");
+    $('#startButton').toggleClass("active");
+    $('#countryList').toggleClass("active");
+    $('td').addClass("hover");
     trivia.createGrid();
-    if ($('#timer_div').attr("class") === "active"){
+    trivia.storeAnswer();
+    trivia.createQuestionElement();
+
+  if ($('#timer_div').attr("class") === "active"){
       trivia.timer();
       $('#timer_div').toggleClass("active");
   }
-    else {
+
+
+  else {
+      console.log("addSencondsToTimer");
       trivia.addSecondstoTimer();
     }
+
   }
+
+  $('#next').on('click',function(){
+    trivia.checkAnswer();
+
+  });
+
+$('#PlayAgainButton').on('click', function(){
+  $('#next').toggleClass("active");
+  $('#lostGame').toggleClass("hidden");
+  $('#countryList').toggleClass("active");
+  $('td').addClass("hover");
+  trivia.createGrid();
+  trivia.storeAnswer();
+  trivia.createQuestionElement();
+
+if ($('#timer_div').attr("class") === "active"){
+    trivia.timer();
+    $('#timer_div').toggleClass("active");
+}
+
+
+else {
+    console.log("addSencondsToTimer");
+    trivia.addSecondstoTimer();
+  }
+
+});
+
+$('#PlayAgainWin').on('click', function(){
+  $('#next').toggleClass("active");
+  $('#winGame').toggleClass("hidden");
+  $('#countryList').toggleClass("active");
+  $('td').addClass("hover");
+  trivia.createGrid();
+  trivia.storeAnswer();
+  trivia.createQuestionElement();
+
+if ($('#timer_div').attr("class") === "active"){
+    trivia.timer();
+    $('#timer_div').toggleClass("active");
+}
+
+
+else {
+    console.log("addSencondsToTimer");
+    trivia.addSecondstoTimer();
+  }
+
+});
+
+
+
+
 });
--- js/game.js
+++ js/game.js
@@ -1,62 +1,66 @@
-// var HistoryTriviaWordSearch = function(){
-//   alaphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// };
+
 var TriviaWordSearch = function(){
-    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     this.timeRemaining = 30;
+    this.currentQuestion = 0;
+    this.playerAnswer = '';
+    this.questionContainer = $('#questionContainer');
+    this.currentLetter = undefined;
+    this.previousLetter = undefined;
+    this.gameOver = false;
     this.questions = [
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
+        question: "Which country has the most people behind bars?",
         answer: "USA",
         level: "1/10"
       },
       {
-        question: "Brazil was once a colony of which European country?",
-        answer: "Portugal",
+        question: "Which country has the most overweight population?",
+        answer: "NAURU",
         level: "2/10"
       },
       {
-        question: "Event triggering US involvement in WWII?",
-        answer: "Pearl Harbor",
+        question: "Which country covers the most time zones? Including oversees territories.",
+        answer: "FRANCE",
         level: "3/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "Which country has the most lakes in the world?",
+        answer: "CANADA",
+        level: "4/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "Which country produces the most oxygen?",
+        answer: "RUSSIA",
+        level: "5/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "Which country is the largest country with no farms?",
+        answer: "SINGAPORE",
+        level: "6/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "All of the pandas in the world are on loan from this country. ",
+        answer: "CHINA",
+        level: "7/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "Which country is smaller in area than Central Park in New York City?",
+        answer: "MONACO",
+        level: "8/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "Which country has more of it's people living abroad?",
+        answer: "MALTA",
+        level: "9/10"
       },
       {
-        question: "Which country was the first to aqcuire an atomic bomb?(abbreviation)",
-        answer: "USA",
-        level: "1/10"
+        question: "Which country has the least densly populated population?",
+        answer: "MONGOLIA",
+        level: "10/10"
       },
     ];
 };
+
 
 
 TriviaWordSearch.prototype.createGrid = function () {
@@ -65,7 +69,7 @@ rows = [];
 cols = null;
   for (var i=0; i <=8; i++){
     cols = "";
-    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var alphabet = 'USANAURUFRANCECANADASINGAPOREMALTAMONGOLIARUSSIAMONACOCHINA';
     for(var h=0; h <= 8; h++){
       var random = parseInt(Math.random()* alphabet.length);
         var letter = alphabet.charAt(random);
@@ -75,43 +79,123 @@ cols = null;
      rows.push('<tr>' + cols + '</tr>');
   }
   document.getElementById('letterBox').innerHTML += rows.join("");
-
 };
+
+
 
 TriviaWordSearch.prototype.timer = function () {
-  // console.log(this.timeRemaining);
-  // var seconds_left = this.timeRemaining;
   var self = this;
-
   var interval = setInterval(function() {
-
       document.getElementById('timer_div').innerHTML = self.timeRemaining--;
-
-      if (self.timeRemaining <= 0)
+      if (self.timeRemaining < 0)
       {
-          
           clearInterval(interval);
+          trivia.resetLose();
 
       }
-
-
   }, 1000);
-
 };
+
+
+
 
 TriviaWordSearch.prototype.addSecondstoTimer = function(){
   var seconds_added = 10;
   this.timeRemaining = this.timeRemaining + seconds_added;
+};
 
-  // var interval = setInterval(function() {
-  //
-  //     document.getElementById('timer_div').innerHTML = --seconds_left;
-  //
-  //     if (seconds_left === 0)
-  //     {
-  //       clearInterval(interval);
-  //
-  //     }
-  // }, 1000);
 
+
+TriviaWordSearch.prototype.createQuestionElement = function(){
+
+  $('#question').append(this.questions[this.currentQuestion].question);
+
+};
+
+
+TriviaWordSearch.prototype.storeAnswer = function() {
+  var self = this;
+
+  $("td").click(function(){
+    console.log("this: ", this);
+    if($(this).hasClass('chosen')){
+      var letter = $(this).html();
+      var arr = self.playerAnswer.split("");
+      var index = arr.indexOf(letter);
+      arr.splice(index, 1);
+      self.playerAnswer = arr.join("");
+      console.log(self.playerAnswer);
+      $(this).toggleClass("chosen");
+    } else {
+      self.playerAnswer += ($(this).html());
+
+      $(this).toggleClass("chosen");
+      console.log(self.playerAnswer);
+    }
+
+  });
+  printedAnswer = self.playerAnswer;
+$(self.playerAnswer).html();
+};
+
+
+TriviaWordSearch.prototype.checkAnswer = function(){
+  if(this.playerAnswer === this.questions[this.currentQuestion].answer){
+    $('#' + (this.currentQuestion + 1) + '').addClass('found');
+    this.nextQuestion();
+    this.addSecondstoTimer();
+  }
+  else {
+    console.log("cleaning playerAnswer");
+    this.playerAnswer = '';
+    this.createGrid();
+    this.storeAnswer();
+
+  }
+};
+
+
+TriviaWordSearch.prototype.nextQuestion = function(){
+  $('#question').text('');
+    this.currentQuestion += 1;
+    if(this.currentQuestion < this.questions.length){
+
+      $('#question').text(this.questions[this.currentQuestion].question);
+      this.createGrid();
+      this.storeAnswer();
+      this.checkAnswer();
+  }
+  else {
+    trivia.resetWin();
+  }
+};
+
+TriviaWordSearch.prototype.resetLose = function(){
+  this.currentQuestion = 0;
+  this.playerAnswer = '';
+  this.timeRemaining = 30;
+  $('td').remove();
+  $('#question').text('');
+  $('#countryList').toggleClass("active");
+  $('#next').toggleClass("active");
+  // $('#startButton').toggleClass("active");
+  $('#timer_div').toggleClass("active");
+  $('#lostGame').toggleClass("hidden");
+
+  console.log("RESET");
+};
+
+TriviaWordSearch.prototype.resetWin = function(){
+  this.currentQuestion = 0;
+  this.playerAnswer = '';
+  this.timeRemaining = 30;
+  $('td').remove();
+  $('#question').text('');
+  $('#countryList').toggleClass("active");
+  $('#next').toggleClass("active");
+  // $('#startButton').toggleClass("active");
+  $('#timer_div').toggleClass("active");
+  $('#winGame').toggleClass("hidden");
+
+  console.log("RESET");
 };
